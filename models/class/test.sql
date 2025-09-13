87462c693a2587247efa437d9754cfdb297a2278
hhh hhh <rakeshsnow877@gmail.com>
2025-09-13 04:53:39 +0000

    first_try

---
+++ models/class/test.sql
@@ -0,0 +1,10 @@
+
+
+
+wITH matches AS
+    (SELECT row_number() over(order by team_name) AS id, t.*
+     FROM first_db.sql_practice.teams t)
+SELECT team.team_name AS team, opponent.team_name AS opponent
+FROM matches team
+JOIN matches opponent ON team.id <> opponent.id
+ORDER BY team
